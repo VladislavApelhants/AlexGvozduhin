94a95f136b793050c49f8e865db69d70e9e1dd17
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-07-28 19:41:04 +0300

    Fix commit

--- src/js/cat-api-service.js
+++ src/js/cat-api-service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-//! Added default API_KEY
+// Added default API_KEY
 axios.defaults.headers.common['x-api-key'] =
-  'live_WU3qNW7HCGgc9ygOkysxRpFXZrESXftSjzKXfEP6AiHj02s1uf3NyjBqcYglzxCa ';
+  'live_WU3qNW7HCGgc9ygOkysxRpFXZrESXftSjzKXfEP6AiHj02s1uf3NyjBqcYglzxCa';
 
 // ------------- Created clss CatApiService -------------
 export class CatApiService {
@@ -11,14 +11,14 @@ export class CatApiService {
   }
 
   //* Added Private Class Elements
-  #BASE_URL = 'https://api.thecatapi.com/v1/';
-  #END_POINTS = {
+  BASE_URL = 'https://api.thecatapi.com/v1/';
+  END_POINTS = {
     BREEDS: 'breeds',
     IMAGES_SEARCH: 'images/search',
   };
 
   //* Private function to handle server response (The Cat API)
-  #handleResponse(response) {
+  handleResponse(response) {
     if (response.status !== 200) {
       throw new Error(response.status);
     }
@@ -28,15 +28,13 @@ export class CatApiService {
 
   // Function which return all the cats (promise)
   fetchBreeds() {
-    const url = `${this.#BASE_URL}${this.#END_POINTS.BREEDS}`;
-    return axios.get(url).then(this.#handleResponse);
+    const url = `${this.BASE_URL}${this.END_POINTS.BREEDS}`;
+    return axios.get(url).then(this.handleResponse);
   }
 
   // Function which return the cat (promise) by ID
   fetchCatByBreed(breedId) {
-    const url = `${this.#BASE_URL}${
-      this.#END_POINTS.IMAGES_SEARCH
-    }?breed_ids=${breedId}`;
-    return axios.get(url).then(this.#handleResponse);
+    const url = `${this.BASE_URL}${this.END_POINTS.IMAGES_SEARCH}?breed_ids=${breedId}`;
+    return axios.get(url).then(this.handleResponse);
   }
 }
