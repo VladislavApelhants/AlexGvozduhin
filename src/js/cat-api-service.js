336d57dd4769c05e85f41947741968a890a692f2
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-07-31 22:40:06 +0300

    Update cat-api-service.js

--- src/js/cat-api-service.js
+++ src/js/cat-api-service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
-axios.defaults.headers.common['x-api-key'] =
-  'live_WU3qNW7HCGgc9ygOkysxRpFXZrESXftSjzKXfEP6AiHj02s1uf3NyjBqcYglzxCa';
+axios.defaults.headers.common['x-api-key'] = 'live_WU3qNW7HCGgc9ygOkysxRpFXZrESXftSjzKXfEP6AiHj02s1uf3NyjBqcYglzxCa';
+// const KEY = 'live_WU3qNW7HCGgc9ygOkysxRpFXZrESXftSjzKXfEP6AiHj02s1uf3NyjBqcYglzxCa';
 
 // --------- Function which return all the cats (promise) ---------
 export async function fetchBreeds() {
