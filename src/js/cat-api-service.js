ddf72c41c1b891a15a2a18c7eb85d822f4c9b236
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-07-31 21:54:27 +0300

    Update cat-api-service.js

--- src/js/cat-api-service.js
+++ src/js/cat-api-service.js
@@ -5,33 +5,27 @@ axios.defaults.headers.common['x-api-key'] =
   'live_WU3qNW7HCGgc9ygOkysxRpFXZrESXftSjzKXfEP6AiHj02s1uf3NyjBqcYglzxCa';
 
 //* Added Private Class Elements
-axios.defaults.baseURL = 'https://api.thecatapi.com/v1/'
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 // const BASE_URL = 'https://api.thecatapi.com/v1/';
-const END_POINTS_BREEDS = 'breeds';
-const END_POINTS_IMAGES_SEARCH = 'images/search';
+// const END_POINTS_BREEDS = 'breeds';
+// const END_POINTS_IMAGES_SEARCH = 'images/search';
 
 // --------- Function which return all the cats (promise) ---------
-export function fetchBreeds() {
-  const url = `breeds`;
-  return axios
-    .get(url)
-    .then(resp => {
-      return resp.data;
-    })
-    .catch(error => {
-      throw new Error('Error details:', error.message);
-    });
+export async function fetchBreeds() {
+  try {
+    const resp = await axios.get(`breeds`);
+    return resp.data;
+  } catch (error) {
+    throw new Error('Error details:', error.message);
+  }
 }
 
 // --------- Function which return the cat (promise) by ID ---------
-export function fetchCatByBreed(breedId) {
-  const url = `images/search?breed_ids=${breedId}`;
-  return axios
-    .get(url)
-    .then(resp => {
-      return resp.data;
-    })
-    .catch(error => {
-      throw new Error('Details of cat search Error:', error.message);
-    });
+export async function fetchCatByBreed(breedId) {
+  try {
+    const resp = await axios.get(`images/search?breed_ids=${breedId}`);
+    return resp.data;
+  } catch (error) {
+    throw new Error('Details of cat search Error:', error.message);
+  }
 }
